0652b0a76d8d1ee064bc9d2134596c2700ce0b60
fcoutinhoj <fcoutinhoj@gmail.com>
2025-03-02 16:54:05 -0300

    workspace

--- Aula01/src/Aula01/Aula01.java
+++ Aula01/src/Aula01/Aula01.java
@@ -1,7 +1,24 @@
 package Aula01;
+
+//import java.util.Scanner;
+
 public class Aula01 {
 	public static void main(String[] args) {
 		
+		//Scanner s = new Scanner(System.in);
+		
+		//String modelo = null;
+		//String cor = null;
+		//float ponta = 0;
+		
+		//System.out.println("modelo: ");
+		//modelo = s.next();
+		//System.out.println("Cor: ");
+		//cor = s.next();
+		//System.out.println("ponta: ");
+		//ponta = s.nextFloat();
+				
+		//Caneta c1 = new Caneta(modelo, cor, ponta);
 		Caneta c1 = new Caneta("bic", "azul", 0.5f);
 
 		//c1.setModelo("Bic");
---
+++ Aula02/src/Aula02/Aula02.java
@@ -0,0 +1,9 @@
+package Aula02;
+public class Aula02 {
+	public static void main(String[] args) {
+		
+		System.out.println("ola");
+		
+	}
+
+}
